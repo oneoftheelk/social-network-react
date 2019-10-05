a2ce1a2f749a3b463a0735d32bef9de7a40c9bb3
Raman Tamilin <oneoftheelk@gmail.com>
2019-10-05 23:48:26 +0300

    Updates

---
+++ precache-manifest.66c0e7b7837c9131534aa64aa99075c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4add7ec6b865d281fdda48693881dfb0",
+    "url": "/social-network-react/index.html"
+  },
+  {
+    "revision": "846cdc935c465b3f0508",
+    "url": "/social-network-react/static/css/main.ab8fa5f0.chunk.css"
+  },
+  {
+    "revision": "668d226bccc35fdf49af",
+    "url": "/social-network-react/static/js/2.3b8774a7.chunk.js"
+  },
+  {
+    "revision": "846cdc935c465b3f0508",
+    "url": "/social-network-react/static/js/main.9d162c91.chunk.js"
+  },
+  {
+    "revision": "a3560e63b54ad07e19ac",
+    "url": "/social-network-react/static/js/runtime~main.5884e785.js"
+  },
+  {
+    "revision": "fc047347b17f7df7ff288d78c8c281cf",
+    "url": "/social-network-react/static/media/avatar.fc047347.png"
+  },
+  {
+    "revision": "c368e429214ce3bd2b862ea3db044eac",
+    "url": "/social-network-react/static/media/preloader.c368e429.gif"
+  }
+]);
